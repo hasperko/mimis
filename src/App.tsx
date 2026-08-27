8824f539aca0946b23bd646d5a4b8fd4d41d3475
hsprzzz <mrlng.simon@gmail.com>
2026-08-27 14:01:56 +0200

    feat: add @vercel/analytics dependency and integrate Analytics component in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import './App.css'
 import GameCanvas from './GameCanvas'
 import Welcome from './Welcome'
 import { useState } from 'react'
+import { Analytics } from '@vercel/analytics/next';
 
 function App() {
   const [tiktokUsername, setTiktokUsername] = useState<string | null>(null);
-  return tiktokUsername === null ? (
-    <Welcome onStart={setTiktokUsername} />
-  ) : (
+  return <>
+    {tiktokUsername === null ? (
+      <Welcome onStart={setTiktokUsername} />
+    ) : (
       <GameCanvas username={tiktokUsername} onError={() => setTiktokUsername(null)} />
-    );
+    )}
+    <Analytics />
+  </>;
 }
 
 export default App
